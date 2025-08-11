b1abe7c34ce33552f1806a40dcbba563a47ad3f7
pedroalma <pedroalma554@gmail.com>
2025-08-11 11:51:59 -0300

    1.0.0

---
+++ src/components/Tarefas.jsx
@@ -0,0 +1,51 @@
+import React, {use,useEffect,useState} from "react";
+
+function Tarefas() {
+    const [input, setInput] = useState("");
+    const [tarefas, setTarefas] = useState([
+        "Pagar conta de luz",
+        "Pagar conta de água",
+        "Assistir aulas",
+    ]);
+
+    useEffect(() => {
+        const tarefasStorage = localStorage.getItem("@tarefas");
+        if (tarefasStorage) {
+            setTarefas(JSON.parse(tarefasStorage));
+        }
+    }, []);
+    
+    useEffect(() => {
+        localStorage.setItem("@tarefas", JSON.stringify(tarefas));
+    }, [tarefas]);
+
+    function cadastrarTarefa(e) {
+        e.preventDefault();
+        setTarefas([...tarefas, input]);
+        setInput("");
+    }
+    return(
+        <div>
+            <h1>Cadastro de tarefas</h1>
+            <form onSubmit={cadastrarTarefa}>
+                <label htmlFor="nome">Nome da tarefa:</label>
+                <input 
+                    placeholder="Digite uma tarefa"
+                    type="text" 
+                    autoFocus
+                    value={input} 
+                    onChange={(e) => setInput(e.target.value)} 
+                />
+                <button type="submit">Cadastrar</button>
+            </form> 
+            <div>
+                <ul>
+                    {tarefas.map((tarefas) => (
+                        <li key={tarefas}>{tarefas}</li>
+                    ))}
+                </ul>
+            </div>   
+        </div>
+    );
+}
+export default Tarefas;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Formulario from './components/Formulario'
-import MyForm from './components/test'
+import Timer from './app'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Formulario />
+    <Timer/>
   </StrictMode>,
 )
